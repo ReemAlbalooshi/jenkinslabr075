a3fc178a3d5f2f540b6c846b8d532fbd89e1b7b6
ReemAlbalooshi <reemalbalooshi20@gmail.com>
2024-08-01 11:16:06 +0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main( String[] arga ) {
+         public static void main( String[] args) {
+
+        System.out.println("Author: Reem");
+        System.out.println("Jenkins Project - Explaining Git Project Compilation");
+
+    }
+}
